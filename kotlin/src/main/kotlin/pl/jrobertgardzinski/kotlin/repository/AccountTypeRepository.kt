15f7140cf8a4fe3479ee109053fd0fbe2cb10ae8
Robert Gardzinski <jrobertgardzinski@gmail.com>
2020-11-11 15:54:53 +0100

    Update AccountTypeRepository.kt

--- kotlin/src/main/kotlin/pl/jrobertgardzinski/kotlin/repository/AccountTypeRepository.kt
+++ kotlin/src/main/kotlin/pl/jrobertgardzinski/kotlin/repository/AccountTypeRepository.kt
@@ -1,7 +1,7 @@
 package pl.jrobertgardzinski.kotlin.repository
 
 import org.springframework.data.mongodb.repository.MongoRepository
-import pl.jrobertgardzinski.kotlin.csv.model.AccountType
+import pl.jrobertgardzinski.kotlin.model.AccountType
 
 @ExperimentalStdlibApi
 interface AccountTypeRepository : MongoRepository<AccountType, Int> {
